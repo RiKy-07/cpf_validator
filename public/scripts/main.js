5a7406114ae707f9343fce2f207d1b1fd6356b9f
RiKy-07 <ramosruikiamy2121@gmail.com>
2025-10-12 22:56:42 +0100

    progresso ate agora

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,5 @@
 let cpf = "123.456.789-87";
+// let cpf = "183.328.348-13";
 let novaLista = [];
 
 novaLista = cpf.replace(/\D/g, "").split("").map(Number);
@@ -9,86 +10,61 @@ function remove_last_twodigits (){
 
 function calculate_tenthdigit(){
     let lista2 = remove_last_twodigits();
-    let numeroAtual;
-    let mult;
-    let inverted = [];
-
-    for(i= 0 ; i<lista2.length; i++){
-        numeroAtual = lista2[lista2.length-1-i];
-        inverted.push(numeroAtual);
-
-        for(n=0; n<inverted.length; n++){
-            if (i >= 2){
-            mult = inverted*i;
-        }
-        
-        }console.log(inverted[n])
+    let inverted = [...lista2.reverse()];
+    let soma = 0;
+    let tenth_digit;
+    for(i= 0 ; i<inverted.length; i++){
+        if (i>=0){
+            mult = inverted[i]*(i+2);
+            soma += mult;
+        }      
     }
-
+    if(soma%11 < 2){
+        tenth_digit=0;
+    }else{
+        tenth_digit = 11 - (soma%11);
+    }
+    lista2.reverse();
+    lista2.push(tenth_digit);
+    return lista2;
 }
 
-calculate_tenthdigit();
+function calculate_eleventhdigit (){
+    let lista2 = calculate_tenthdigit();
 
+    let inverted = [...lista2.reverse()];
+    let soma = 0;
+    let eleventh_digit;
+    for(i= 0 ; i<inverted.length; i++){
+        if (i>=0){
+            mult = inverted[i]*(i+2);
+            soma += mult;
+        }      
+    }
+    if(soma%11 < 2){
+        eleventh_digit=0;
+    }else{
+        eleventh_digit = 11 - (soma%11);
+    }
+    lista2.reverse();
+    lista2.push(eleventh_digit);
 
+    return lista2;
+}
 
+let ideal_cpf = calculate_eleventhdigit();
+let cpf_equal;
 
-// for (let i = 0; i <= cpf.length-1; i++) {
-//     let numero = Number(cpf[i]);
-//     console.log(numero)
-//     if (!Number.isNaN(numero)) {
-//         novaLista.push(numero);
-//     }
-// }
+for(i=0; i<novaLista.length; i++){
+        if(novaLista[i] === ideal_cpf[i]){
+            cpf_equal = true;
+        }else{
+            cpf_equal = false; 
+        }
+}
 
-// console.log(novaLista)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function remove_dots (){
-//     return cpf.replaceAll(".", "");
-// }
-
-// function remove_hyphen(){
-//     return cpf.replace("-","");
-// }
-
-// /*function convert_to_string(){
-//     const cpf_without_points= remove_dots();
-//     const cpf_without_points = remove_hyphen();
-    
-     
-      
-//      return  Sting(cpf)
-// }*/
-
-// function convert_to_string(){
-//     cpf = remove_dots();
-//     cpf = remove_hyphen();
-//     return cpf;
-// }
-
-// function convert_to_number(){
-//    const cpfNumber= Number(cpf)
-//          console.log(typeof cpfNumber);
-//          cpf = cpfNumber
-//     return cpf;
-// }
-
-// function convert_to_array(){
-//     cpf = convert_to_number();
-//     cpf = cpf.split('')
-//     console.log(cpf);
-// }
-
-// convert_to_array();
+if(cpf_equal == true){
+    console.log("Valid CPF");
+}else{
+    console.log("Invalid CPF");
+}
