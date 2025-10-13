e6503934042c64b5da2205ef0dc62c86a1bb59c2
RiKy-07 <ramosruikiamy2121@gmail.com>
2025-10-13 12:58:57 +0100

    progresso ate agora

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,28 +1,53 @@
-let cpf = "123.456.789-87";
-// let cpf = "183.328.348-13";
-let novaLista = [];
-
-novaLista = cpf.replace(/\D/g, "").split("").map(Number);
+function get_input(){
+    let cpf = document.querySelector('#input').value;    
+    let novaLista = []
+    novaLista = cpf.replace(/\D/g, "").split("").map(Number);
+    return novaLista;
+}
 
 function remove_last_twodigits (){
+    novaLista = get_input();
     return novaLista.slice(0,-2);
 }
 
-function calculate_tenthdigit(){
+function reject_sequences() {
+
+    let novaLista = get_input();
+    let i = 0;
+    
+    let true_list = [];
+
+    for( i = 0; i < novaLista.length; i++) {
+
+        if( novaLista[i] == novaLista [0]) {
+            true_list.push(true);
+        }
+    }
+
+    return true_list;
+
+}
+
+function calculate_tenthdigit() {
+
     let lista2 = remove_last_twodigits();
     let inverted = [...lista2.reverse()];
+
     let soma = 0;
     let tenth_digit;
-    for(i= 0 ; i<inverted.length; i++){
-        if (i>=0){
-            mult = inverted[i]*(i+2);
+
+    for( i = 0; i < inverted.length; i++) {
+        
+        if ( i >= 0 ) {
+
+            mult = inverted[i] * ( i + 2 );
             soma += mult;
         }      
     }
     if(soma%11 < 2){
         tenth_digit=0;
     }else{
-        tenth_digit = 11 - (soma%11);
+        tenth_digit = 11 - (soma%11);    
     }
     lista2.reverse();
     lista2.push(tenth_digit);
@@ -39,7 +64,7 @@ function calculate_eleventhdigit (){
         if (i>=0){
             mult = inverted[i]*(i+2);
             soma += mult;
-        }      
+        }
     }
     if(soma%11 < 2){
         eleventh_digit=0;
@@ -52,19 +77,30 @@ function calculate_eleventhdigit (){
     return lista2;
 }
 
-let ideal_cpf = calculate_eleventhdigit();
-let cpf_equal;
+function verify_cpf(){
 
-for(i=0; i<novaLista.length; i++){
-        if(novaLista[i] === ideal_cpf[i]){
-            cpf_equal = true;
-        }else{
-            cpf_equal = false; 
+    let ideal_cpf = calculate_eleventhdigit();
+    let novaLista = get_input();
+    let true_list = reject_sequences();
+
+    let cpf_equal;
+
+        for(i=0; i<novaLista.length; i++){
+
+                if( true_list.length == 11 ) {
+                    cpf_equal = false;
+
+                }else if( novaLista[i] === ideal_cpf[i] ) {
+                     cpf_equal = true;
+
+                }else{
+                    cpf_equal = false;
+                }
         }
-}
 
-if(cpf_equal == true){
-    console.log("Valid CPF");
-}else{
-    console.log("Invalid CPF");
+        if(cpf_equal == true){
+            window.alert("Valid CPF");
+        }else{
+            window.alert("Invalid CPF");
+        }
 }
