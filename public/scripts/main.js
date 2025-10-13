70a6c7f666c56a1788d6700e1cf594a6bc0ccd50
RiKy-07 <ramosruikiamy2121@gmail.com>
2025-10-13 13:24:44 +0100

    Latest and last version of inex.html

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,106 +1,101 @@
-function get_input(){
-    let cpf = document.querySelector('#input').value;    
-    let novaLista = []
-    novaLista = cpf.replace(/\D/g, "").split("").map(Number);
-    return novaLista;
+function get_input() {
+  let cpf = document.querySelector("#input").value;
+  let novaLista = [];
+  novaLista = cpf.replace(/\D/g, "").split("").map(Number);
+  return novaLista;
 }
 
-function remove_last_twodigits (){
-    novaLista = get_input();
-    return novaLista.slice(0,-2);
+function remove_last_twodigits() {
+  novaLista = get_input();
+  return novaLista.slice(0, -2);
 }
 
 function reject_sequences() {
+  let novaLista = get_input();
+  let i = 0;
 
-    let novaLista = get_input();
-    let i = 0;
-    
-    let true_list = [];
+  let true_list = [];
 
-    for( i = 0; i < novaLista.length; i++) {
-
-        if( novaLista[i] == novaLista [0]) {
-            true_list.push(true);
-        }
+  for (i = 0; i < novaLista.length; i++) {
+    if (novaLista[i] == novaLista[0]) {
+      true_list.push(true);
     }
+  }
 
-    return true_list;
-
+  return true_list;
 }
 
 function calculate_tenthdigit() {
+  let lista2 = remove_last_twodigits();
+  let inverted = [...lista2.reverse()];
 
-    let lista2 = remove_last_twodigits();
-    let inverted = [...lista2.reverse()];
+  let soma = 0;
+  let tenth_digit;
 
-    let soma = 0;
-    let tenth_digit;
-
-    for( i = 0; i < inverted.length; i++) {
-        
-        if ( i >= 0 ) {
-
-            mult = inverted[i] * ( i + 2 );
-            soma += mult;
-        }      
+  for (i = 0; i < inverted.length; i++) {
+    if (i >= 0) {
+      mult = inverted[i] * (i + 2);
+      soma += mult;
     }
-    if(soma%11 < 2){
-        tenth_digit=0;
-    }else{
-        tenth_digit = 11 - (soma%11);    
-    }
-    lista2.reverse();
-    lista2.push(tenth_digit);
-    return lista2;
+  }
+  if (soma % 11 < 2) {
+    tenth_digit = 0;
+  } else {
+    tenth_digit = 11 - (soma % 11);
+  }
+  lista2.reverse();
+  lista2.push(tenth_digit);
+  return lista2;
 }
 
-function calculate_eleventhdigit (){
-    let lista2 = calculate_tenthdigit();
+function calculate_eleventhdigit() {
+  let lista2 = calculate_tenthdigit();
 
-    let inverted = [...lista2.reverse()];
-    let soma = 0;
-    let eleventh_digit;
-    for(i= 0 ; i<inverted.length; i++){
-        if (i>=0){
-            mult = inverted[i]*(i+2);
-            soma += mult;
-        }
+  let inverted = [...lista2.reverse()];
+  let soma = 0;
+  let eleventh_digit;
+  for (i = 0; i < inverted.length; i++) {
+    if (i >= 0) {
+      mult = inverted[i] * (i + 2);
+      soma += mult;
     }
-    if(soma%11 < 2){
-        eleventh_digit=0;
-    }else{
-        eleventh_digit = 11 - (soma%11);
-    }
-    lista2.reverse();
-    lista2.push(eleventh_digit);
+  }
+  if (soma % 11 < 2) {
+    eleventh_digit = 0;
+  } else {
+    eleventh_digit = 11 - (soma % 11);
+  }
+  lista2.reverse();
+  lista2.push(eleventh_digit);
 
-    return lista2;
+  return lista2;
 }
 
-function verify_cpf(){
+function verify_cpf() {
+  let ideal_cpf = calculate_eleventhdigit();
+  let novaLista = get_input();
+  let true_list = reject_sequences();
 
-    let ideal_cpf = calculate_eleventhdigit();
-    let novaLista = get_input();
-    let true_list = reject_sequences();
+  let cpf_equal;
 
-    let cpf_equal;
-
-        for(i=0; i<novaLista.length; i++){
-
-                if( true_list.length == 11 ) {
-                    cpf_equal = false;
-
-                }else if( novaLista[i] === ideal_cpf[i] ) {
-                     cpf_equal = true;
-
-                }else{
-                    cpf_equal = false;
-                }
+  try {
+      for ( i = 0; i < novaLista.length; i++) {
+        if (true_list.length == 11) {
+          cpf_equal = false;
+        } else if (novaLista[i] === ideal_cpf[i]) {
+          cpf_equal = true;
+        } else {
+          cpf_equal = false;
         }
+      }
 
-        if(cpf_equal == true){
-            window.alert("Valid CPF");
-        }else{
-            window.alert("Invalid CPF");
-        }
+      if (cpf_equal == true) {
+      } else {
+        console.log(new Error("CPF invalido"))
+      }
+
+  } catch (err) {
+    console.error(err)
+  }
+
 }
